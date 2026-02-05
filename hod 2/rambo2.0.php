f63d0fb03363d91957860548339dde99a79f16da
Samuel <suteksamuel02@gmail.com>
2026-02-05 13:11:42 +0100

    add rambooo

--- hod 2/rambo2.0.php
+++ hod 2/rambo2.0.php
@@ -1,7 +1,8 @@
+
 <?php
 if($_SERVER["REQUEST_METHOD"] === "POST"){
 $pole = [
-    "Rambo 1" => ["zaner" => "akčne",
+    "Rambo 1" => ["zaner" => "akcne",
                  "hl_postava" => "Sylvester Stallone",
                  "rok_vydania" => 1982
                 ],
@@ -10,9 +11,12 @@ $pole = [
                  "rok_vydania" => 1976],
     "Forest Gump" => ["zaner" => "drama",
                       "hl_postava" => "Tom Hanks",
-                      "rok_vydania" => 1994]
-        ];
-// print_r($pole);
+                      "rok_vydania" => 1994],
+        
+    "Kurier" => ["zaner" => "akcne",
+                 "hl_postava" => "Jason Statham",
+                  "rok_vydania" => 2002]
+               ];
 
 
 }
@@ -27,14 +31,9 @@ $pole = [
         body{
             width: 100vw;
             height: 100vh;
-            /* display: flex;   */
-            /* flex-direction:row; */
-        }
-        p{
-            /* justify-self:end; */
         }
         form{
-            height:50px;
+            height:150px;
             width:230px;
         }
     </style>
@@ -45,25 +44,35 @@ $pole = [
             <label for="nazov">Názov</label>
             <input type="text" name="nazov">
         </div>
+        
+        <div>
+            <label for="akcne">Akčné</label>
+            <input type="radio" name="radijko"id="akcne" value="akcne" checked><br>
+            <label for="drama">Dráma</label>
+            <input type="radio" name="radijko" id="drama" value="drama"><br>
+            <label for="komedia">Komédia</label>
+            <input type="radio" name="radijko" id="komedia" value= "komedia"><br>
+        </div>
+        
         <button type="submit" class="send_button">Send</button>
     </form>
         <?php
         $kontrol = FALSE;
+        $zaner = $_POST["radijko"];
         foreach($pole as $key => $value){
-            if($key === $_POST["nazov"]){
-                echo "<div>";
-                echo $value["zaner"];
-                echo "</div>";
-                echo "<div>";
-                echo $value["hl_postava"];
-                echo "</div>";
-                echo "<div>";
-                echo $value["rok_vydania"];
-                echo "</div>";
-                $kontrol = TRUE;      
-            }
-
+            if($key === $_POST["nazov"] & $value["zaner"] === $zaner){
+                echo $value["zaner"], "<br>";
+                echo $value["hl_postava"], "<br>";
+                echo $value["rok_vydania"], "<br>";
+                $kontrol = TRUE;   
+            } 
         }
+
+        // if($value["zaner"] === $_POST["radijko"]){
+        //     echo "aaaaaaaaa";
+        // }
+        
+        // echo $_POST["radijko"];
         if($kontrol === FALSE){
             echo "Film nie je v databáze";
         }
