6d98f82ab22394527e10961af8368a99d9a20ac1
Samuel <suteksamuel02@gmail.com>
2026-03-18 11:12:39 +0100

    push dtbz

--- hod 6/Kniha.php
+++ hod 6/Kniha.php
@@ -2,11 +2,12 @@
 
 class Kniha
 {
-    public string $nazov;
-    public string $autor;
-    public int $rok_vydania;
+    private int $id;
+    private string $nazov;
+    private string $autor;
+    private int $rok_vydania;
 
-    public int $stav;
+    private int $stav;
 
     public function __construct($nazov, $autor, $rok_vydania, $stav){
         $this->nazov = $nazov;
@@ -15,30 +16,25 @@ class Kniha
         $this->stav = $stav;
     }
 
-    public function setNazov($nazov){
-        $this->nazov = $nazov;
-    }
+
     public function getNazov(){
         return $this->nazov;
     }
+    public function setId($id){
+        $this->id = $id;
+    }
 
-    public function setAutor($autor){
-        $this->autor = $autor;
+    public function getId(){
+        return $this->id;
     }
     public function getAutor(){
         return $this->autor;
     }
 
-    public function setRok_vydania($rok_vydania){
-        $this->rok_vydania = $rok_vydania;
-    }
     public function getRok_vydania(){
         return $this->rok_vydania;
     }
 
-    public function setStav($stav){
-        $this->stav = $stav;
-    }
     public function getStav(){
         return $this->stav;
     }
--- hod 6/index.php
+++ hod 6/index.php
@@ -5,18 +5,40 @@ include "Database.php";
 
 $spojenie = new Database();
 $db = $spojenie->nadviazSpojenie();
-var_dump($db);
+
+
 if(!$db){
     die(" Databaza nie je pripojena");
 }
 
-$kniznica = [];
 
-$svet = new Kniha("Svet", "Janko Hruska", 1897, 1);
-$kniznica[] = $svet;
+$sql = "SELECT * FROM knihy";
+$stmt = $db->query($sql);
 
-$auto = new Kniha("Auto", "Stano Mak", 2000, 1);
-$kniznica[] = $auto;
+if($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["action"] && $_POST["action"] === "delete")){
+    $sql = "DELETE FROM knihy WHERE id = :id";
+
+    $stmt = $db->prepare($sql);
+
+    return $stmt->execute([
+        ":id" => $_POST["kniha_id"]
+    ]);
+
+    header("Location:index.php");
+    exit();
+}
+
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+    $kniha = null;
+
+    $kniha = new Kniha($row["nazov"], $row["autor"], (int)$row["rok_vydania"], (int)$row["stav"]);
+
+    if($kniha){
+        $kniha->setId($row["id"]);
+        $kniznica[] = $kniha;
+    }
+};
 
 ?>
 
@@ -25,6 +47,7 @@ $kniznica[] = $auto;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
@@ -36,6 +59,7 @@ $kniznica[] = $auto;
             <th>Autor</th>
             <th>Rok vydania</th>
             <th>Stav</th>
+            <th>Action</th>
         </tr>
 
         <?php foreach($kniznica as $kniha):?>
@@ -51,6 +75,13 @@ $kniznica[] = $auto;
                 </td>
                 <td>
                     <?= $kniha->getStav() ?>
+                </td>
+                <td>
+                    <form action="index.php" method="POST">
+                        <input type="hidden" name="action" value="delete">
+                        <input type="hidden" name="kniha_id" value="<?php $kniha->getId();?>">
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
                 </td>                
             </tr>
         <?php endforeach; ?>
